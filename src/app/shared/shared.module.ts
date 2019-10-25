9a4535d62fd7a212ca65bcd2612d3573b6bcff75
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-25 22:44:52 +0300

    Create empty navbar and footer component in shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [FooterComponent, NavbarComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [FooterComponent, NavbarComponent]
+})
+export class SharedModule {}
