efc0cfcd2c4801bef1a639578c2e4ab7d62c6389
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-25 23:49:42 +0300

    Add initial navbar implementation

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatButtonModule, MatToolbarModule, MatMenuModule],
+  exports: [MatButtonModule, MatToolbarModule, MatMenuModule]
 })
 export class MaterialModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [FooterComponent, NavbarComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   exports: [FooterComponent, NavbarComponent]
 })
 export class SharedModule {}
